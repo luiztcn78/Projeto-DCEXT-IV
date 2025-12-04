a2709c4810b1ee384ab7a46524970cad10a89831
Lucas Cassiano <lucasccesar05@gmail.com>
2025-12-03 21:31:35 -0300

    fix: atempt to fix CORS error

--- app/database/session.py
+++ app/database/session.py
@@ -1,4 +1,3 @@
-# app/database/session.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,14 +6,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Use a URL completa do .env
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Se ainda não estiver definida, use esta:
 if not DATABASE_URL:
-    DATABASE_URL = "postgresql://postgres:ahdifhavajxb@db.evxthciabgwbqsvtzclw.supabase.co:5432/postgres"
+    DATABASE_URL = (
+        "postgresql://postgres:ahdifhavajxb@db.evxthciabgwbqsvtzclw.supabase.co:5432/postgres"
+        "?sslmode=require"
+    )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
